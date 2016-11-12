7414500cf91ac8264004253d856bfe7f54582c71
mikelking <mikel.king@olivent.com>
2016-11-12 15:43:29 -0500

    Added the basic configuration files. Will determine if they need to be stripped out into their own project later.

---
+++ staging-conf.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+	This file contains the configuration specific to the staging site
+	@version 1.3
+*/
+require(__DIR__ . '/server-conf-base.php');
+
+class ServerConfig implements ServerConfigBase {
+
+	public function set_db_credentials() {
+		$this->password = '%%PASSWORD%%';
+		$this->db       = '%%DATABASE%%';
+		$this->user     = '%%USER%%';
+		$this->host     = '%%HOST%%';
+	}
+
+	public function set_keys_and_salts() {
+		$this->auth_key         = '%%AUTHKEY%%';
+		$this->secure_auth_key  = '%%SECAUTHKEY%%';
+		$this->logged_in_key    = '%%LOGGEDINKEY%%';
+		$this->nonce_key        = '%%NONCE%%';
+		$this->auth_salt        = '%%AUTHSALT%%';
+		$this->secure_auth_salt = '%%SECAUTHSALT%%';
+		$this->logged_in_salt   = '%%LOGGEDINSALT%%';
+		$this->nonce_salt       = '%%NONCESALT%%';
+		$this->wp_cache_salt    = '%%CACHESALT%%';
+	}
+
+	public function set_caching_options() {
+		$this->wp_caching = self::ENABLED;
+
+		$this->memcached_servers = array(
+			'default' => array(
+				'127.0.0.1:11211'
+			)
+		);
+	}
+
+	/*
+		refer to: https://codex.wordpress.org/Debugging_in_WordPress
+	*/
+	public function set_debug_options() {
+		$this->wpdbg        = self::DISABLED;
+		$this->dbg_log      = self::DISABLED;
+		$this->show_errors  = self::DISABLED;
+		$this->script_dbg   = self::DISABLED;
+		$this->save_queries = self::DISABLED;
+		$this->mke_api      = self::ENABLED;
+	}
+
+
+	public function set_logging_options() {
+		$this->default_timezone_set = self::DEFAULT_TIMEZONE;
+		$this->error_level          = E_ALL;
+		$this->reporting_level      = self::REPORTING_LEVEL;
+		$this->mke_api_response     = self::ENABLED;
+		$this->mke_api_request      = self::ENABLED;
+
+	}
+
+	public function set_hyperdb_cfg() {
+		$db_cfg = array(
+			'host'          => $this->host,
+			'user'          => $this->user,
+			'password'      => $this->password,
+			'name'          => $this->db,
+			'write'         => 1,
+			'read'          => 1,
+			'dataset'       => 'global',
+			'timeout'       => 0.2,
+			'lag_threshold' => 2,
+		);
+	}
+}
+
