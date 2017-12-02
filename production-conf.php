adc7cf94c2ab317ee921a3ae371fb18e28a6a767
mikelking <mikel.king@olivent.com>
2017-12-01 21:29:06 -0500

    Implemanted the build system.

--- production-conf.php
+++ production-conf.php
@@ -4,72 +4,159 @@
 	This file contains the configuration specific to the production site
 	@version 1.3
 */
-require(__DIR__ . '/server-conf-base.php');
+require( '/server-conf-base.php' );
 
 class ServerConfig extends ServerConfigBase {
+	const SITENAME         = 'olivent.net';
+	const SITE_SALT        = 'olivent_rule$';
+	const PROTOCOL         = 'http';
+	const PROTOCOL_DELIM   = '://';
+	const DEFAULT_TIMEZONE = 'America/New_York';
 
-	public function set_db_credentials() {
-		$this->password = '%%PASSWORD%%';
-		$this->db       = '%%DATABASE%%';
-		$this->user     = '%%USER%%';
-		$this->host     = '%%HOST%%';
+	// DB Access
+	const DB_PASSWORD = 'P0pc0rn78%32';
+	const DB_USER     = 'oliventdbadm';
+	const DB_NAME     = 'olivent_network';
+	const DB_HOST     = 'localhost';
+
+	// Caching
+	const WP_CACHE = true;
+
+	// Debugging
+	const DEBUG        = false;
+	const LOG_ERRORS   = false;
+	const SHOW_ERRORS  = false;
+	const SCRIPT_DEBUG = false;
+	const SAVE_QUERIES = false;
+
+	// Setup Logging
+	const REPORTING_LEVEL  = 0;
+	const ERROR_LEVEL      = '';
+
+	// CMS Settings
+	const AUTO_SAVE_DELAY       = 86400; // seconds
+	const WP_TURN_OFF_ADMIN_BAR = false;
+	const WP_POST_REVISIONS     = 10;
+
+	// File System
+	const BLOCK_FILE_EDITS = false;
+	const BLOCK_FILE_MODS  = false;
+	const FS_CHMOD_DIR     = 0775;
+	const FS_CHMOD_FILE    = 0664;
+	const DEFAULT_UMASK    = 0002;
+
+	// System Updates
+	const AUTOMATIC_UPDATER_DISABLED = true;
+	const WP_AUTO_UPDATE_CORE        = false;
+
+	// Security SSL not necessary if you entire site is HTTPS
+	const COOKIE_DOMAIN = '';
+	const FORCE_SSL_LOGIN = false;
+	const FORCE_SSL_ADMIN = false;
+
+	// Performance
+	const WP_MEMORY_LIMIT     = '512M';
+	const CONCATENATE_SCRIPTS = false;
+
+	// MulitSite
+	const MULTISITE           = true;
+	const WP_ALLOW_MULTISITE  = true;
+	const SUBDOMAIN_INSTALL   = true;
+	const DOMAIN_CURRENT_SITE = 'olivent.net';
+	const PATH_CURRENT_SITE   = '/';
+	const SITE_ID_CURRENT_SITE = 1;
+	const BLOG_ID_CURRENT_SITE = 1;
+
+	// Misc
+	const SUNRISE         = false;
+	const DISABLE_WP_CRON = false;
+
+	public $db_cfg;
+	public $memcached_servers;
+
+
+	public function __construct() {
+		$this->set_db_credentials();
+		$this->set_keys_and_salts();
+		$this->set_caching_options();
+		$this->set_hyperdb_cfg();
+		$this->set_logging_options();
+		$this->set_debug_options();
 	}
 
-	public function set_keys_and_salts() {
-		$this->auth_key         = '%%AUTHKEY%%';
-		$this->secure_auth_key  = '%%SECAUTHKEY%%';
-		$this->logged_in_key    = '%%LOGGEDINKEY%%';
-		$this->nonce_key        = '%%NONCE%%';
-		$this->auth_salt        = '%%AUTHSALT%%';
-		$this->secure_auth_salt = '%%SECAUTHSALT%%';
-		$this->logged_in_salt   = '%%LOGGEDINSALT%%';
-		$this->nonce_salt       = '%%NONCESALT%%';
-		$this->wp_cache_salt    = '%%CACHESALT%%';
+	public function get_version() {
+		return( static::class . ' Version: ' . static::VERSION . PHP_EOL );
 	}
 
-public function set_caching_options() {
-		$this->wp_caching = self::ENABLED;
-
-		$this->memcached_servers = array(
-			'default' => array(
-					'127.0.0.1:11211'
-			)
-		);
+	public function get_salted_hash( $key ) {
+		return( sha1( static::SITE_SALT . $key ) );
 	}
+
+	public function get_auth_key() {
+		return( $this->get_salted_hash( 'auth_key' ) );
+	}
+
+	public function get_secure_auth_key() {
+		return( $this->get_salted_hash( 'secure_auth_key' ) );
+	}
+
+
+	public function get_logged_in_key() {
+		return( $this->get_salted_hash( 'logged_in_key' ) );
+	}
+
+	public function get_nonce_key() {
+		return( $this->get_salted_hash( 'nonce_key' ) );
+	}
+
+	public function get_auth_salt() {
+		return( $this->get_salted_hash( 'auth_salt' ) );
+	}
+
+	public function get_secure_auth_salt() {
+		return( $this->get_salted_hash( 'secure_auth_salt' ) );
+	}
+
+	public function get_logged_in_salt() {
+		return( $this->get_salted_hash( 'logged_in_salt' ) );
+	}
+
+
+	public function get_nonce_salt() {
+		return( $this->get_salted_hash( 'nonce_salt' ) );
+	}
+
+	public function get_cache_salt() {
+		return( $this->get_salted_hash( 'cache_salt' ) );
+	}
+
+	abstract public function set_caching_options();
+
+	abstract public function set_logging_options();
 
 	/*
-		refer to: https://codex.wordpress.org/Debugging_in_WordPress
+		HyperDB is tricky in that you should have distinct credentials
+		for server in the database cluster
 	*/
-	public function set_debug_options() {
-		$this->wpdbg        = self::DISABLED;
-		$this->dbg_log      = self::DISABLED;
-		$this->show_errors  = self::DISABLED;
-		$this->script_dbg   = self::DISABLED;
-		$this->save_queries = self::DISABLED;
-		$this->mke_api      = self::ENABLED;
-	}
+	abstract public function set_hyperdb_cfg();
 
-
-	public function set_logging_options() {
-		$this->default_timezone_set = self::DEFAULT_TIMEZONE;
-		$this->error_level          = E_ALL;
-		$this->reporting_level      = self::REPORTING_LEVEL;
-		$this->mke_api_response     = self::ENABLED;
-		$this->mke_api_request      = self::ENABLED;
-
-	}
-
-	public function set_hyperdb_cfg() {
-		$db_cfg = array(
-			'host'          => $this->host,
-			'user'          => $this->user,
-			'password'      => $this->password,
-			'name'          => $this->db,
-			'write'         => 1,
-			'read'          => 1,
-			'dataset'       => 'global',
-			'timeout'       => 0.2,
-			'lag_threshold' => 2,
+	public function check_caching_options() {
+		return(
+				$this->wp_caching ||
+				(isset($this->memcached_servers) && is_array($this->memcached_servers))
 		);
 	}
+
+	/**
+	 * @return string
+	 */
+	public function get_sitename() {
+		$schema = static::PROTOCOL . self::PROTOCOL_DELIM;
+		if ( CLI_Controller::is_cli() && $this->sitename !== '' ) {
+			return( $schema . static::SITENAME );
+		} else {
+			return( $schema . $_SERVER['HTTP_HOST'] );
+		}
+	}
+
 }
