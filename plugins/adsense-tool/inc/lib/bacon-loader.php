5189350eab0fb48bfd7c03519b4ffe2233ae4b47
Mikel King <mikel.king@olivent.com>
2018-08-04 09:48:33 -0400

    imporve names and fixed validation errros

--- plugins/adsense-tool/inc/lib/bacon-loader.php
+++ plugins/adsense-tool/inc/lib/bacon-loader.php
@@ -6,12 +6,12 @@
 $self = basename( __FILE__, '.php' );
 $source_path = __DIR__ . '/bacon/';
 $iterator = new DirectoryIterator( $source_path );
-while( $iterator->valid() && $iterator->key() != 18 ) {
-    $file = $iterator->current();
-    if ( ! $iterator->isDot() && $iterator->key() != 2 && $iterator->key() != 3 ) {
-        //echo $iterator->key() . " => " . $file->getFilename() . "\n";
-    require( $source_path .  $file->getFilename() );
-    }
-    $iterator->next();
+while ( $iterator->valid() && $iterator->key() != 18 ) {
+	$file = $iterator->current();
+	if ( ! $iterator->isDot() && $iterator->key() != 2 && $iterator->key() != 3 ) {
+		//echo $iterator->key() . " => " . $file->getFilename() . "\n";
+		require( $source_path . $file->getFilename() );
+	}
+	$iterator->next();
 }
 
