b22056d866347403bcb95625b6471d63da533f05
Mikel King <mikel.king@olivent.com>
2018-11-28 15:00:56 -0500

    updated the footer in the old theme for now.

--- src/themes/jafdip-3rd_style/footer.php
+++ src/themes/jafdip-3rd_style/footer.php
@@ -22,7 +22,7 @@
 	get_sidebar( 'footer' );
 ?>
 
-			<div id="site-info"> copyright &copy; 2007,2008,2009,2010 & 2011  
+			<div id="site-info"> copyright &copy; 2018  
 				<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 					<?php bloginfo( 'name' ); ?>
 				</a> 
